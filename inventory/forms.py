3090b150551b465831649b5a710969c00eddd154
jrggementiza <jrggementiza@gmail.com>
2019-02-18 17:08:24 +0800

    worked on addItem component on footer of /inventory

--- inventory/forms.py
+++ inventory/forms.py
@@ -4,13 +4,31 @@ from .models import Item
 
 
 class AddItemForm(ModelForm):
+    item_name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control input-sm',
+            'placeholder': 'ex. Tumbler',
+        }
+    ))
+    item_from = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control input-sm',
+            'placeholder': 'ex. Supplier',
+        }
+    ))
+    item_get_price = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control input-sm',
+            'placeholder': 'ex. 300',
+        }
+    ))
+    item_sell_price = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control input-sm',
+            'placeholder': 'ex. 800',
+        }
+    ))
     class Meta:
         model = Item
         fields = ['item_name', 'item_from',
                   'item_get_price', 'item_sell_price']
-        labels = {
-            'item_name': 'Item Name',
-            'item_from': 'Item From',
-            'item_get_price': 'Item Get Price',
-            'item_sell_price': 'Item Sell Price',
-        }
