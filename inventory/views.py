607a91df42b9377088ee7fecf0d8516880e9ac5b
jrggementiza <jrggementiza@gmail.com>
2019-02-21 17:09:39 +0800

    started working on sales functionality

--- inventory/views.py
+++ inventory/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from .models import Item, Inventory
 from .forms import AddItemForm
 
+from sales.models import Sales
+
 
 def inventory(request):
     form = AddItemForm()
@@ -11,10 +13,24 @@ def inventory(request):
     if request.method == 'POST':
         if 'sellItem' in request.POST:
             selected_item = request.POST.get('sellItem', None)
+            print(type(selected_item))
+            print(selected_item)
+
             if selected_item == None:
                 prompt = 'item not found'
+
+            # Deletes Item on Inventory table
             item_query = Inventory.objects.get(item=selected_item)
-            print(item_query)
+            item_query.delete()
+
+            # Gets Object Instance of Item to Sell
+            item_to_sell = Item.objects.get(item_id=selected_item)
+            print(item_to_sell)
+
+            # TODO: Fix double entry bug
+            # TODO: Add customer table and moda;
+            new_sales_item = Sales(item=item_to_sell)
+            new_sales_item.save()
 
         elif 'editItem' in request.POST:
             selected_item = request.POST.get('editItem', None)
